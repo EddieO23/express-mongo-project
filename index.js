3e30f3c1de6cb9f0925597d0870645727f8e6466
eocampo52 <eocampo52@gmail.com>
2024-09-05 16:17:29 -0500

    finished adding the ability to edit product items

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
 const Product = require("./models/product");
 
@@ -15,10 +16,12 @@ mongoose
     console.log(err);
   });
 
+// MIDDLEWARE
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-
+app.use(methodOverride("_method"));
 app.get("/products", async (req, res) => {
   const products = await Product.find({});
   res.render("products/index", { products });
@@ -32,7 +35,7 @@ app.get("/products/new", (req, res) => {
 app.post("/products", async (req, res) => {
   const newProduct = new Product(req.body);
   await newProduct.save();
-  res.redirect(`/products/${newProduct._id}`)
+  res.redirect(`/products/${newProduct._id}`);
 });
 
 // Get individual item
@@ -40,6 +43,20 @@ app.get("/products/:id", async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
   res.render("products/show", { product });
+});
+
+// Updating an item routes
+
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", { product });
+});
+
+app.put("/products/:id", async (req, res) => {
+  const {id} = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true})
+  res.redirect(`/products/${product._id }`)
 });
 
 app.listen(3000, () => {
