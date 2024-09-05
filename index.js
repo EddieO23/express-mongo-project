ab1b1542f5bfc53be7f2f297fe8954ec680b037d
eocampo52 <eocampo52@gmail.com>
2024-09-05 14:22:51 -0500

    added the ability to get product info individually added the ability to see items on db

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+const express = require("express");
+const app = express();
+const path = require("path");
 const mongoose = require("mongoose");
+
+const Product = require("./models/product");
+
 mongoose
-  .connect("mongodb://127.0.0.1:27017/shopApp")
+  .connect("mongodb://127.0.0.1:27017/farmStand")
   .then(() => {
     console.log("mongo connection open");
   })
@@ -12,14 +15,21 @@ mongoose
     console.log(err);
   });
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.get("/products", async (req, res) => {
+  const products = await Product.find({});
+  res.render('products/index', {products });
+});
 
-app.get('/dog', (req, res) => {
-  res.send('Woof')
+app.get('/products/:id', async (req, res) => {
+  const {id} = req.params
+  const product =  await Product.findById(id)
+  console.log(product)
+  res.render('products/show', {product})
 })
 
 app.listen(3000, () => {
-  console.log('Roger roger.')
-})
+  console.log("Roger roger.");
+});
